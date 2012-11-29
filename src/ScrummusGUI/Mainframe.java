f876482b2de4251127f2f79a331b6d68d4c5daec
Zachary Daniels <zachdaniels@dyn154058.cc.lehigh.edu>
2012-11-29 18:24:53 -0500

    Bug fixes, added sort by year

--- src/ScrummusGUI/Mainframe.java
+++ src/ScrummusGUI/Mainframe.java
@@ -59,11 +59,10 @@ public class Mainframe extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(153, 204, 255));
 
         FilterList.setModel(new javax.swing.AbstractListModel() {
-            String[] strings = { "None", "Most Played", "Most Recent" };
+            String[] strings = { "None", "Most Played", "Most Recent", "Year of Creation" };
             public int getSize() { return strings.length; }
             public Object getElementAt(int i) { return strings[i]; }
         });
-        FilterList.setSelectedIndex(0);
         FilterList.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
             public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
                 FilterListValueChanged(evt);
@@ -181,6 +180,9 @@ public class Mainframe extends javax.swing.JFrame {
         }
         else if(FilterList.getSelectedValue().equals("Most Recent")){
             sortDate();
+        }
+        else if(FilterList.getSelectedValue().equals("Year of Creation")){
+            sortYear();
         }
     }//GEN-LAST:event_FilterListValueChanged
 
@@ -342,6 +344,48 @@ public class Mainframe extends javax.swing.JFrame {
             TreeMultimap<Date,Video> sortedVideos = TreeMultimap.create(Ordering.natural().reverse(), Ordering.natural());
             for(int i = 0; i < videos.size(); i++){
                 sortedVideos.put(videos.get(i).getDateAdded(),videos.get(i));
+            }
+            Collection<Video> sortedVideos2 = (Collection)sortedVideos.values();
+            int i = 0;
+            for(Video video: sortedVideos2){
+                model.addRow(new Object[]{video.getName(),video.getAuthor(),video.getDuration(),video.getGenre()});
+                i++;
+            }
+        }
+    }
+        private void sortYear(){
+        //songs
+        model.setRowCount(0);
+        model.setColumnCount(0);
+        if(selectedMedia == 0){
+            model.addColumn("Song");
+            model.addColumn("Artist");
+            model.addColumn("Time");
+            model.addColumn("Album");
+            model.addColumn("Genre");
+            ArrayList<Song> songs = database.getAllSongs();
+            TreeMultimap<Integer,Song> sortedSongs = TreeMultimap.create(Ordering.natural(), Ordering.natural());
+            for(int i = 0; i < songs.size(); i++){
+                sortedSongs.put(songs.get(i).getYear(),songs.get(i));
+            }
+            Collection<Song> sortedSongs2 = (Collection)sortedSongs.values();
+            int i = 0;
+            for(Song song: sortedSongs2){
+                model.addRow(new Object[]{song.getName(),song.getArtist(),song.getDuration(),song.getAlbum(),song.getGenre()});
+                i++;
+            }
+            
+        }
+        //videos
+        else{
+            model.addColumn("Video");
+            model.addColumn("Author");
+            model.addColumn("Time");
+            model.addColumn("Genre");
+            ArrayList<Video> videos = database.getAllVideos();
+            TreeMultimap<Integer,Video> sortedVideos = TreeMultimap.create(Ordering.natural(), Ordering.natural());
+            for(int i = 0; i < videos.size(); i++){
+                sortedVideos.put(videos.get(i).getYear(),videos.get(i));
             }
             Collection<Video> sortedVideos2 = (Collection)sortedVideos.values();
             int i = 0;
